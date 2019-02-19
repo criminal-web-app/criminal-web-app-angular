a445c39492c0a8e4e786b515e4bfa5f8b1526684
maangeloatienza <maangeloatienza@gmail.com>
2019-02-19 23:22:04 +0800

    Add register functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,35 @@
-let userModule		= angular.module('user-app', ['ui.bootstrap']);
+let userApp		= angular.module("userApp", []);
 
+userApp.service('Api',function($http){
+	return function(){
+		return $http.get('api.json');
+	}
+});
+
+userApp.controller('registerUserController', ($scope,$http,Api)=>{
+	$scope.data = {
+		first_name : $scope.first_name,
+		last_name : $scope.last_name,
+		username : $scope.username,
+		password : $scope.password,
+		phone_number : $scope.phone_number,
+		role_id : $scope.role_id
+	}
+	
+	$scope.registerUser = ()=>{
+		Api()
+		.then(function(api){
+			$http({
+				url : api.data.userLocal,
+				method : 'POST',
+				data : $scope.data
+			})
+			.then(result=>{
+				console.log(result);
+			})
+			.catch(err=>{
+				console.log(err);
+			})
+		})
+	} 
+});
